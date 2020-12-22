15dfccbfb973be221c8ca690c1d7ac673ff3a5d8
MylesSHarris <69069959+MylesSHarris@users.noreply.github.com>
2020-12-21 20:00:06 -0800

    Add files via upload

---
+++ Lab.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Lab
+{
+    public class Lab
+    {
+        static void Main(string[] arguments)
+        {
+            Pizza pizza = new Pizza(1, 0, 2, 1);
+
+            Console.WriteLine(pizza.CalculateCost());
+            Console.WriteLine(pizza);
+        }
+    }
+}
---
+++ Pizza.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Lab
+{
+    public class Pizza
+    {
+        private int size;
+        private int cheeseToppings;
+        private int pepperoniToppings;
+        private int hamToppings;
+
+        public Pizza(int size, int cheeseToppings, int pepperoniToppings, int hamToppings)
+        {
+            this.size = size;
+            this.cheeseToppings = cheeseToppings;
+            this.pepperoniToppings = pepperoniToppings;
+            this.hamToppings = hamToppings;
+        }
+
+        public double CalculateCost()
+        {
+            double cost = 0;
+            if (size == 0) cost += 10;
+            if (size == 1) cost += 12;
+            if (size == 2) cost += 14;
+            cost += (cheeseToppings * 2);
+            cost += (pepperoniToppings * 2);
+            cost += (hamToppings * 2);
+            return cost;
+        }
+
+        public override string ToString()
+        {
+            return $"Pizza[size={size} cheeseToppings={cheeseToppings} pepperoniToppings={pepperoniToppings} hamToppings={hamToppings} cost={CalculateCost()}]";
+        }
+    }
+}
